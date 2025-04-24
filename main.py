fa1a299c0d11fa0ace7e9e72be9cac71a00e5d33
User <ug-fca-gig@ug.gob.com>
2025-04-24 11:57:04 -0500

    se creo una nueva clase de persona

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 if __name__ == '__main__':
-    print_hi('mundo')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    p1 = Person('John', 20)
+    print(p1.age)
+    print(p1.name)
